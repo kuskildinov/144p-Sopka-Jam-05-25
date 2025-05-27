79de31a4a6ade3617c9e339a4aa6ac6ff6d64791
Kuskildin Ural <kuskildinural@gmail.com>
2025-05-27 15:37:15 +0500

    Добавил ввод игрока

---
+++ SopkaJam/Assets/Source/Scripts/Core/Input/DesktopInput.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DesktopInput : IInput
+{
+    private const string HorizontalAxis = "Horizontal";
+    private const string VerticalAxis = "Vertical";
+    public float HorizontalInput()
+    {
+        return Input.GetAxis(HorizontalAxis);
+    }
+    public float VerticalInput()
+    {
+        return Input.GetAxis(VerticalAxis);
+    }
+    public bool Interaction()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    public bool Dash()
+    {
+        return Input.GetMouseButtonDown(1);
+    }
+}
---
+++ SopkaJam/Assets/Source/Scripts/Core/Input/IInput.cs
@@ -0,0 +1,7 @@
+public interface IInput
+{   
+    public float HorizontalInput();
+    public float VerticalInput();
+    public bool Interaction();
+    public bool Dash();
+}
